7d284c3c69c30007f0e2e77c66784c527c33b3b3
Late Lee <li@latelee.org>
2018-09-21 10:11:03 +0800

    update

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ git config user.email "li@latelee.org"
 # add commit timestamp
 git add .
 git commit -m "update"
-git push "https://558e52d0ab4bbe7673e0ac7737acfffd59ee4cac@github.com/latelee/ci.git" master:master
+git push "https://895841413b01ab0a9b0ac3e5839c97393d244d56@github.com/latelee/ci.git" master:master
